e83c01262ecbca58be2cd5eb2e3aeab430a9333d
Alexandre Colella <nozutbm@gmail.com>
2019-12-08 17:12:11 +0100

    add email config info

---
+++ application/config/email.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['newline'] = "\r\n";
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'maildev';
+$config['smtp_port'] = 25;
+$config['smtp_user'] = null;
+$config['smtp_pass'] = null;
+$config['charset'] = 'iso-8859-1';
+$config['mailtype'] = 'html';
