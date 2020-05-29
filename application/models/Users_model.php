53178ef04a02505abff3c5cef1f45ccc6d04d349
Alexandre Colella <nozutbm@gmail.com>
2020-05-30 01:38:21 +0200

    email lib, new actions on waiting podcasts

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -11,4 +11,9 @@ class Users_Model extends CI_Model
         $this->username = $this->input->post("user");
         $this->db->update(self::DB_TABLE, $this, array("id" => $this->input->post("id")));
     }
+
+    public function findOneById($id)
+    {
+        return $this->db->get_where('users', ['id' => $id])->row();
+    }
 }
