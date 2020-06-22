8e1c2b7b4f0f32b8bbc8a61df50d3cceebad3135
Alexandre Colella <nozutbm@gmail.com>
2020-06-22 23:01:46 +0200

    adding media element

--- application/views/medias/episode.php
+++ application/views/medias/episode.php
@@ -1,3 +1,7 @@
 <h2><?php echo $episode->titre ?></h2>
 
 <p><?php echo $episode->description ?></p>
+
+<audio controls style="max-width:100%;">
+    <source src="<?= $episode->lien_mp3 ?>" type="audio/mp3">
+</audio>
