de04e97437b77550a243bd42b338e963371bfdc4
Nemotaku <nemo.emmanuel@gmail.com>
2020-08-05 20:15:46 +0200

    Intégration mediaplayerelement

--- application/views/medias/episode.php
+++ application/views/medias/episode.php
@@ -2,6 +2,9 @@
 
 <p><?php echo $episode->description ?></p>
 
-<audio controls style="max-width:100%;">
+<!-- PLAYER --> 
+<script src="<?php echo base_url('assets/node_modules/mediaelement/build/mediaelement-and-player.min.js') ?>"></script>
+<link rel="stylesheet" href="<?php echo base_url("assets/node_modules/mediaelement/build/mediaelementplayer.css") ?>" />
+<audio controls style="max-width:100%;" class="mejs__player">
     <source src="<?= $episode->lien_mp3 ?>" type="audio/mp3">
 </audio>
